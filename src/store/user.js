419abdcaf2497a1b2b816a294dbe8e16c3e414d4
zz-orb <22373474@buaa.edu.cn>
2024-12-03 21:19:02 +0800

    login

--- src/store/user.js
+++ src/store/user.js
@@ -1,25 +1,30 @@
-// 需要 勿删
 import { defineStore } from "pinia";
 import { ref, computed } from "vue";
-import {login, register, refreshAccessToken, logout, getUserInfo, getUserActionInfo} from "@/api/user.js";
+import { login, register, refreshAccessToken, logout, getUserInfo } from "@/api/user.js";
 import { ElMessage } from 'element-plus';
 import Cookies from 'js-cookie';
 
 export const useUserStore = defineStore('user', () => {
     const userInfo = ref({});
-    const userCollectDishes = ref([]);
-    const userCollectCounters = ref([]);
-    const userCollectCafeterias = ref([]);
-    const userAte = ref([]);
-    const userUpload = ref([]);
     const token = ref('');
+    // 下面的个人信息之后应该在 userLogin 里面获取 对接完成之后可以删除
+    userInfo.value = {
+        nickname: 'Fanziyu',
+        userid: 22373474,
+        gender: 'female',
+        introduction: '这个人很懒，什么都没留下',
+        username: '樊孜昱',
+        email: '123@qq.com',
+        identityLevel: 2,
+    }
+    // 
 
     const userRegister = async ({ email, username, password }) => {
         await register({ email, username, password });
     };
 
-    const userLogin = async (email, password) => {
-        const response = await login({ email, password });
+    const userLogin = async (nickname, password) => {
+        const response = await login({ nickname, password });
 
         if (response.success) {
             // 1. 返回数据
@@ -32,79 +37,22 @@ export const useUserStore = defineStore('user', () => {
             // 3. 获取用户个人信息
             await updateUserBaseInfo();
 
-            // 4. 获取用户行为数据
-            const resp = await getUserActionInfo();
-            userAte.value = resp.data.ateId;
-            userCollectDishes.value = resp.data.collectDishesId;
-            userCollectCounters.value = resp.data.collectCountersId;
-            userCollectCafeterias.value = resp.data.collectCafeteriasId;
-            userUpload.value = resp.data.uploadPostId;
-            return username;
+            return { username }; // 返回包含用户名和角色的对象
         } else {
             return null;
         }
     }
-    userInfo.value.userId = 22373474;
-    userInfo.value.username = "樊孜昱";
-    userInfo.value.email = '123@qq.com';
+
     const updateUserBaseInfo = async () => {
         const res = await getUserInfo();
-
-        userInfo.value.userId = res.data.id;
+        userInfo.value.nickname = res.data.nickname;
+        userInfo.value.userid = res.data.id;
         userInfo.value.gender = res.data.gender;
         userInfo.value.introduction = res.data.introduction;
-        userInfo.value.avatar = res.data.avatar;
         userInfo.value.username = res.data.username;
         userInfo.value.email = res.data.email;
+        userInfo.value.identityLevel = res.data.identityLevel;
     }
-
-    const extendUserInfo = (type, id) => {
-        if (type === 'ate') {
-            userAte.value = [...userAte.value, id];
-        } else if (type === 'dish') {
-            userCollectDishes.value = [...userCollectDishes.value, id];
-        } else if (type === 'counter') {
-            userCollectCounters.value = [...userCollectCounters.value, id];
-        } else if (type === 'cafeteria') {
-            userCollectCafeterias.value = [...userCollectCafeterias.value, id];
-        } else if (type === 'upload') {
-            userUpload.value = [...userUpload.value, id];
-        }
-        else {
-            console.log("extendUserInfo失败: 没有这个类型")
-        }
-    };
-
-    const removeUserInfo = (type, id) => {
-        if (type === 'ate') {
-            const index = userAte.value.indexOf(id);
-            if (index !== -1) {
-                userAte.value.splice(index, 1);
-            }
-        } else if (type === 'dish') {
-            const index = userCollectDishes.value.indexOf(id);
-            if (index !== -1) {
-                userCollectDishes.value.splice(index, 1);
-            }
-        } else if (type === 'counter') {
-            const index = userCollectCounters.value.indexOf(id);
-            if (index !== -1) {
-                userCollectCounters.value.splice(index, 1);
-            }
-        } else if (type === 'cafeteria') {
-            const index = userCollectCafeterias.value.indexOf(id);
-            if (index !== -1) {
-                userCollectCafeterias.value.splice(index, 1);
-            }
-        } else if (type === 'upload') {
-            const index = userUpload.value.indexOf(id);
-            if (index !== -1) {
-                userUpload.value.splice(index, 1);
-            }
-        } else {
-            console.log("removeUserInfo失败: 没有这个类型")
-        }
-    };
 
     const userLogout = async () => {
         try {
@@ -130,11 +78,6 @@ export const useUserStore = defineStore('user', () => {
 
     const refreshLocal = () => {
         userInfo.value = {};
-        userCollectDishes.value = [];
-        userCollectCounters.value = [];
-        userCollectCafeterias.value = [];
-        userAte.value = [];
-        userUpload.value = [];
         token.value = '';
 
         // 清理本地存储和 cookie
@@ -142,12 +85,10 @@ export const useUserStore = defineStore('user', () => {
         Cookies.remove('myRefreshToken');
     }
 
-    const changeInfo = ({username, email, gender, introduction, avatar}) => {
-        userInfo.value.username = username;
+    const changeInfo = ({ email, gender, introduction }) => {
         userInfo.value.email = email;
         userInfo.value.gender = gender;
         userInfo.value.introduction = introduction;
-        userInfo.value.avatar = avatar;
     };
 
 
@@ -169,31 +110,24 @@ export const useUserStore = defineStore('user', () => {
     };
 
     const headers = computed(() => {
-      return {
-        Authorization: `Bearer ${token.value}`,
-      };
+        return {
+            Authorization: `Bearer ${token.value}`,
+        };
     });
 
     const setToken = (newToken) => {
         token.value = newToken;
         localStorage.setItem('token', newToken);
     };
-    
+
     return {
         userInfo,
+        userRegister,
         userLogin,
+        updateUserBaseInfo,
         userLogout,
         refreshLocal,
-        userRegister,
-        updateUserBaseInfo,
-        extendUserInfo,
-        removeUserInfo,
         changeInfo,
-        userCollectDishes,
-        userCollectCounters,
-        userCollectCafeterias,
-        userAte,
-        userUpload,
 
         // 以下是登陆认证等功能函数
         token,
@@ -206,6 +140,6 @@ export const useUserStore = defineStore('user', () => {
     persist: {
         key: 'user-store', // 自定义存储键名
         storage: window.localStorage, // 你可以指定localStorage或sessionStorage
-        paths: ['userInfo', 'userCollectDishes', 'userCollectCounters', 'userCollectCafeterias', 'userAte', 'userUpload', 'token'], // 需要持久化的状态
+        paths: ['userInfo', 'token'], // 需要持久化的状态
     },
 });
